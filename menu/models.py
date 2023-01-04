6b8022519cf75981d411a5f64c1c5e69a6a41191
mathbrasil <matheusbrasilpvh@hotmail.com>
2023-01-04 15:28:57 -0400

    build: creating develop branch

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.TextChoices):
+    DK = "drinks"
+    FD = "foods"
+    DS = "disserts"
+
+
+class Menu(models.Model):
+    class Meta:
+        ordering = ["id"]
+
+    name = models.CharField(max_length=50)
+    category = models.CharField(
+        max_length=8,
+        choices=Categories.choices,
+    )
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.CharField(max_length=250)
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="user"
+    )
+
+    def __repr__(self) -> str:
+        return f"<Menu item [{self.id}] - {self.name}>"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=50)
+    contact = models.CharField(max_length=20)
+    email = models.EmailField(
+        max_length=127,
+        unique=True,
+        error_messages={
+            "unique": "This field must be unique.",
+        },
+    )
