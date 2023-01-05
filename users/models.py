2de84a086a19c7536e0013b853af3c7542527346
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2023-01-04 20:37:25 -0400

    feat: user models, serializer, view

--- users/models.py
+++ users/models.py
@@ -1,8 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
 
 class User(AbstractUser):
-    username = models.CharField(blank=True, default=None, null=True)
-    email = models.EmailField(unique=True)
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        primary_key=True,
+        editable=False,
+    )
+    username = models.CharField(
+        null=True,
+        max_length=150,
+        unique=True,
+    )
+    name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True, max_length=150)
     is_staff = models.BooleanField(default=True)
+    contact = models.CharField(
+        max_length=150,
+        null=True,
+    )
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+class UserView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
