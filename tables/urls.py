534769e6a01d761805159db5ce44bcf53a1d9c44
Giuseppe Dias <giuseppe.dias17@gmail.com>
2023-01-06 21:08:29 -0300

    feat: add tables urls

--- tables/urls.py
+++ tables/urls.py
@@ -3,10 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("tables/",views.TableView.as_view() ),
-    path("tables/<int:pk>/",views.TableDetailView.as_view()),
-    path("tables/<int:pk>/orders", ),
-    path("tables/<int:pk>/orders/<int:order_id>", ),
-    path("tables/<int:pk>/close", ),
-    path("tables/<int:pk>/music", ),
+    path("tables/", views.TableView.as_view()),
+    path("tables/<int:pk>/", views.TableDetailView.as_view()),
+    # path("tables/<int:pk>/orders", ),
+    # path(
+    #     "tables/<int:pk>/orders/<int:order_id>",
+    # ),
+    # path(
+    #     "tables/<int:pk>/close",
+    # ),
+    # path(
+    #     "tables/<int:pk>/music",
+    # ),
 ]
