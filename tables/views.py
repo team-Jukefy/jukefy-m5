de4cc63e310ce1c02bb0cbd4f5585c6cdc0682c7
Giuseppe Dias <67605920+galio17@users.noreply.github.com>
2023-01-07 11:04:23 -0300

    Feature/create table route (#10)
    * feat: add relation 1-1 to table model with user model
    
    * refactor: remove unnecessary create method to table serializer
    
    * feat: add tables urls
    
    * feat: add post route in table view
    
    * feat: add migrations with relation table_user
    
    * refactor: use black format to code

--- tables/views.py
+++ tables/views.py
@@ -1,17 +1,37 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+import ipdb
+from django.utils.crypto import get_random_string
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import AccessToken
+
+from users.models import User
 
 from .models import Table
 from .serializers import TableSerializer
 
 
 class TableView(generics.ListCreateAPIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
     queryset = Table.objects.all()
     serializer_class = TableSerializer
+
+    user = User.objects.create(
+        username=get_random_string(8),
+        email=get_random_string(8),
+        password=get_random_string(8),
+    )
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.user)
+
+    def create(self, request, *args, **kwargs):
+        super().create(request, *args, **kwargs)
+
+        token = AccessToken.for_user(self.user)
+        token["table_id"] = self.user.table.id
+
+        return Response({"token": str(token)})
 
 
 class TableDetailView(generics.RetrieveUpdateDestroyAPIView):
