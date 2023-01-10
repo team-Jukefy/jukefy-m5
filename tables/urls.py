56379eb5b3452493f56e3a7484fce3f1840d7425
mathbrasil <matheusbrasilpvh@hotmail.com>
2023-01-10 13:09:58 -0400

    fix: urls fixing

--- tables/urls.py
+++ tables/urls.py
@@ -7,16 +7,12 @@ from . import views
 urlpatterns = [
     path("tables/", views.TableView.as_view()),
     path("tables/<int:pk>/", views.TableDetailView.as_view()),
-    # path("tables/<int:pk>/orders", ),
-    # path(
-    #     "tables/<int:pk>/orders/<int:order_id>",
-    # ),
-    # path(
-    #     "tables/<int:pk>/music",
-    # ),
     path("tables/<int:pk>/orders/", views.TableOrderView.as_view()),
     path(
         "tables/<int:table_id>/orders/<int:pk>", order_views.OrderDetailView.as_view()
     ),
     path("tables/<int:pk>/close/", views.TableCloseView.as_view()),
+    # path(
+    #     "tables/<int:pk>/music",
+    # ),
 ]
